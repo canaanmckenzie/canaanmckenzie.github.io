df9aa4b46ec890037b3abb209650680cd5dbec6e
Canaan <cjmckenz@uvm.edu>
2021-10-04 19:13:36 -0400

    please work

--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  #get 'home/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'home#index'
-  get 'home/about'
-  get 'home/projects'
-  get 'home/blog'
-end
